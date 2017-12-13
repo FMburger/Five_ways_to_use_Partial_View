4b6bf7bf0696caa4999b426b7d1a8631b2df2b88
WillardKang <cofession321@hotmail.com>
2017-12-13 12:09:49 +0800

    upload
    Five ways to use Partial View

---
+++ how_to_use_Partal_View/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace how_to_use_Partal_View.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Method1()
+        {
+            return View();
+        }
+
+        public ActionResult Method2()
+        {
+            return View();
+        }
+
+        public ActionResult Method3()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public PartialViewResult M3_Child()
+        {
+            return PartialView("_PartialView");
+        }
+
+        public ActionResult Method4()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public PartialViewResult M4_Child()
+        {
+            return PartialView("_PartialView");
+        }
+
+        public ActionResult Method5()
+        {
+            return View();
+        }
+        public ActionResult M5_Partial_View()
+        {
+            return PartialView("_PartialView");
+        }
+    }
+}
---
+++ how_to_use_Partal_View/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="how_to_use_Partal_View.MvcApplication" Language="C#" %>
